91aa8fa32cf4847963393fbd1429d8a15993a561
sachiko.kimura <sachiko.0212.o.k@gmail.com>
2024-05-10 22:54:43 +0900

    Employeeモデル、EmployeeViewモデル作成

---
+++ src/main/java/actions/views/EmployeeView.java
@@ -0,0 +1,35 @@
+package actions.views;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+//従業員情報について画面の入力値・出力値を扱う
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeView {
+    //id
+    private Integer id;
+
+    private String code;
+
+    private String name;
+
+    private String password;
+
+    private Integer adminFlag;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private Integer deleteFlag;
+}
+
