327f1ed71d1eafbe41ea23185d1b26ad94d14bc3
sachiko.kimura <sachiko.0212.o.k@gmail.com>
2024-05-11 14:27:45 +0900

    ログインアクションの作成

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import constants.MessageConst;
+import constants.PropertyConst;
 import services.EmployeeService;
 
 
@@ -37,5 +40,32 @@ public void showLogin() throws ServletException, IOException {
     }
     forward(ForwardConst.FW_LOGIN);
 
+}
+
+//ログイン処理
+public void login() throws ServletException, IOException {
+    String code = getRequestParam(AttributeConst.EMP_CODE);
+    String plainPass = getRequestParam(AttributeConst.EMP_PASS);
+    String pepper = getContextScope(PropertyConst.PEPPER);
+//有効な従業員か認証する
+    Boolean isValidEmployee = service.validateLogin(code, plainPass, pepper);
+
+if(isValidEmployee) {
+
+    if(checkToken()) {
+//ログインした従業員のDBデータを取得する
+        EmployeeView ev = service.findOne(code, plainPass, pepper);
+        //セッションにログインした従業員を設定
+        putSessionScope(AttributeConst.LOGIN_EMP,ev);
+        putSessionScope(AttributeConst.FLUSH,MessageConst.I_LOGINED.getMessage());
+        redirect(ForwardConst.ACT_TOP,ForwardConst.CMD_INDEX);
+    }
+   } else {
+       putRequestScope(AttributeConst.TOKEN,getTokenId());
+       putRequestScope(AttributeConst.LOGIN_ERR,true);
+       putRequestScope(AttributeConst.EMP_CODE,code);
+
+       forward(ForwardConst.FW_LOGIN);
+   }
 }
 }
