db07791272bea937554726f930282988e9973c73
sachiko.kimura <sachiko.0212.o.k@gmail.com>
2024-05-12 09:44:49 +0900

    日報newアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -1,6 +1,7 @@
 package actions;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -11,55 +12,48 @@ import constants.ForwardConst;
 import constants.JpaConst;
 import services.ReportService;
 
-/**
- * 日報に関する処理を行うActionクラス
- *
- */
 public class ReportAction extends ActionBase {
-
     private ReportService service;
 
-    /**
-     * メソッドを実行する
-     */
+    //メソッド実行
     @Override
     public void process() throws ServletException, IOException {
+        service = new  ReportService();
 
-        service = new ReportService();
-
-        //メソッドを実行
         invoke();
         service.close();
     }
 
-    /**
-     * 一覧画面を表示する
-     * @throws ServletException
-     * @throws IOException
-     */
+    //一覧画面を表示
     public void index() throws ServletException, IOException {
 
-        //指定されたページ数の一覧画面に表示する日報データを取得
         int page = getPage();
         List<ReportView> reports = service.getAllPerPage(page);
 
-        //全日報データの件数を取得
         long reportsCount = service.countAll();
 
-        putRequestScope(AttributeConst.REPORTS, reports); //取得した日報データ
-        putRequestScope(AttributeConst.REP_COUNT, reportsCount); //全ての日報データの件数
-        putRequestScope(AttributeConst.PAGE, page); //ページ数
-        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE); //1ページに表示するレコードの数
+        putRequestScope(AttributeConst.REPORTS, reports);
+        putRequestScope(AttributeConst.REP_COUNT, reportsCount);
+        putRequestScope(AttributeConst.PAGE, page);
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE);
 
-        //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
+        //
         String flush = getSessionScope(AttributeConst.FLUSH);
         if (flush != null) {
-            putRequestScope(AttributeConst.FLUSH, flush);
+            putRequestScope(AttributeConst.FLUSH,flush);
             removeSessionScope(AttributeConst.FLUSH);
         }
-
-        //一覧画面を表示
         forward(ForwardConst.FW_REP_INDEX);
     }
+    //entryNew
+    public void entryNew() throws ServletException,IOException {
+        putRequestScope(AttributeConst.TOKEN,getTokenId());
 
+        //今日の日付を設定
+        ReportView rv= new ReportView();
+        rv.setReportDate(LocalDate.now());
+        putRequestScope(AttributeConst.REPORT, rv);
+
+        forward(ForwardConst.FW_REP_NEW);
+    }
 }
